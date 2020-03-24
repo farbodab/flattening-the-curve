420301f16f8d597f245e13ebbed78c0013e7c7f5
Andrew Lee <alee@klick.com>
2020-03-24 16:58:00 -0400

    Started graph, added resizing

--- src/app/components/home-graph/home-graph.component.ts
+++ src/app/components/home-graph/home-graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -10,15 +10,33 @@ export class HomeGraphComponent implements OnInit {
 
     @Input() graph_data : any;
 
-    ngOnInit() {
-        console.log(this.graph_data);
+    graph = null;
+
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+      this.redraw();
     }
 
-    public graph = {
-        data: [
-            { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
-            { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
-        ],
-        layout: {width: 500, height: 500, title: 'A Fancy Plot'}
-    };
+    ngOnInit() {
+        this.redraw();
+    }
+
+    private redraw() {
+        const ontario = this.graph_data['Ontario'];
+        const italy = this.graph_data['Italy'];
+        const korea = this.graph_data['South Korea'];
+
+        this.graph = null;
+
+        this.graph = {
+            data: [
+                { x: Object.keys(ontario), y: Object.values(ontario), type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
+                { x: Object.keys(italy), y: Object.values(italy), type: 'scatter', mode: 'lines+points', marker: {color: 'blue'} },
+                { x: Object.keys(korea), y: Object.values(korea), type: 'scatter', mode: 'lines+points', marker: {color: 'green'} },
+            ],
+            layout: {title: 'A Fancy Plot'}
+        };
+    }
+   
 }
