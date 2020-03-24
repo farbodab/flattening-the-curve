8a7762aa0d635ff95a13fde7daa4d55bd22c4cf8
Andrew Lee <alee@klick.com>
2020-03-24 15:43:32 -0400

    Updated home graph loading

--- src/app/components/home-graph/home-graph.component.ts
+++ src/app/components/home-graph/home-graph.component.ts
@@ -3,10 +3,8 @@ import * as d3 from 'd3';
 
 @Component({
     selector: 'app-home-graph',
-    template: `
-    <plotly-plot [data]="graph.data" [layout]="graph.layout"
-       [useResizeHandler]="true" [style]="{position: 'relative', width: '100%', height: '100%'}">
-    </plotly-plot>`,
+    templateUrl: './home-graph.component.html',
+    styleUrls: ['./home-graph.component.scss']
 })
 export class HomeGraphComponent {
     public graph = {
