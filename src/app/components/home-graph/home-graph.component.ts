387d9c0628ce16e88ad24378ce083de07b724452
unknown <farbod.ab@hotmail.ca>
2020-03-24 15:35:54 -0400

    added plotly

--- src/app/components/home-graph/home-graph.component.ts
+++ src/app/components/home-graph/home-graph.component.ts
@@ -2,21 +2,18 @@ import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
-  selector: 'app-home-graph',
-  templateUrl: './home-graph.component.html',
-  styleUrls: ['./home-graph.component.scss']
+    selector: 'app-home-graph',
+    template: `
+    <plotly-plot [data]="graph.data" [layout]="graph.layout"
+       [useResizeHandler]="true" [style]="{position: 'relative', width: '100%', height: '100%'}">
+    </plotly-plot>`,
 })
-export class HomeGraphComponent implements OnInit {
-
-  @Input() graph_data : any;
-
-  constructor() { }
-
-  ngOnInit() {
-    this.create_chart();
-  }
-
-  create_chart() {
-    
-  }
+export class HomeGraphComponent {
+    public graph = {
+        data: [
+            { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
+            { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+        ],
+        layout: {width: 500, height: 500, title: 'A Fancy Plot'}
+    };
 }
