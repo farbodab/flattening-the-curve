6979f1125c2b830d07139411d14e0933696c2d69
Andrew Lee <alee@klick.com>
2020-03-24 15:02:22 -0400

    Style updates

--- src/app/components/home-graph/home-graph.component.ts
+++ src/app/components/home-graph/home-graph.component.ts
@@ -8,9 +8,6 @@ import * as d3 from 'd3';
 })
 export class HomeGraphComponent implements OnInit {
 
-  @ViewChild('chart', {static:false})
-  private chart_container : ElementRef;
-
   @Input() graph_data : any;
 
   constructor() { }
@@ -20,25 +17,6 @@ export class HomeGraphComponent implements OnInit {
   }
 
   create_chart() {
-    d3.select('svg').remove();
-
-    const keys = Object.keys(this.graph_data);
-    const values = keys.map( key => this.graph_data[key] )
-
-    console.log(values);
-    
-    var margin = {top: 20, right: 20, bottom: 30, left: 40};
-
-    const element = this.chart_container.nativeElement;
-    const data = this.graph_data;
-
-    const svg = d3.select(element).append('svg')
-      .attr('width', element.offsetWidth)
-      .attr('height', element.offsetHeight);
-
-    const contentWidth = element.offsetWidth - margin.left - margin.right;
-    const contentHeight = element.offsetHeight - margin.top - margin.bottom;
-
     
   }
 }
