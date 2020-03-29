348d8542ca3ae77bc6f6643ecf8354291f19b65f
unknown <farbod.ab@hotmail.ca>
2020-03-29 12:12:20 -0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,27 +13,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { StoryComponent } from './pages/story/story.component';
 import { ResourcesComponent } from './pages/resources/resources.component';
 import { ConnectComponent } from './pages/connect/connect.component';
-import { HomeGraphComponent } from './components/home-graph/home-graph.component';
-import { HomeGraphOntComponent } from './components/home-graph-ont/home-graph-ont.component';
-import { PlotlyViaCDNModule } from 'angular-plotly.js';
 import { AnalysisComponent } from './pages/analysis/analysis.component';
 import { DataComponent } from './pages/data/data.component';
-import { NumCasesTotalGraphComponent } from './components/num-cases-total-graph/num-cases-total-graph.component';
-import { NumCasesTimeGraphComponent } from './components/num-cases-time-graph/num-cases-time-graph.component';
-import { DataCapacityConservativeResAvailComponent } from './components/data-capacity-conservative-res-avail/data-capacity-conservative-res-avail.component';
-import { DataCapacityItalyResAvailComponent } from './components/data-capacity-italy-res-avail/data-capacity-italy-res-avail.component';
-import { DataCapacityConservativeWaitingResComponent } from './components/data-capacity-conservative-waiting-res/data-capacity-conservative-waiting-res.component';
-import { DataCapacityItalyWaitingResComponent } from './components/data-capacity-italy-waiting-res/data-capacity-italy-waiting-res.component';
-import { DataTestingOverTimeComponent } from './components/data-testing-over-time/data-testing-over-time.component';
-import { DataTestProportionByResultComponent } from './components/data-test-proportion-by-result/data-test-proportion-by-result.component';
-import { DataRegionalMapComponent } from './components/data-regional-map/data-regional-map.component';
 
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule, ScreenTrackingService } from '@angular/fire/analytics';
-
-PlotlyViaCDNModule.plotlyVersion = '1.49.4'; // can be `latest` or any version number (i.e.: '1.40.0')
-PlotlyViaCDNModule.plotlyBundle = 'basic'; // optional: can be null (for full) or 'basic', 'cartesian', 'geo', 'gl3d', 'gl2d', 'mapbox' or 'finance'
 
 
 @NgModule({
@@ -44,8 +29,6 @@ PlotlyViaCDNModule.plotlyBundle = 'basic'; // optional: can be null (for full) o
     StoryComponent,
     ResourcesComponent,
     ConnectComponent,
-    HomeGraphComponent,
-    HomeGraphOntComponent,
     AnalysisComponent,
     DataComponent,
   ],
@@ -53,7 +36,6 @@ PlotlyViaCDNModule.plotlyBundle = 'basic'; // optional: can be null (for full) o
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
     BrowserModule,
-    PlotlyViaCDNModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
