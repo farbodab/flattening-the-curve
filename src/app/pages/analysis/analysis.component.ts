79000dea35cdda9cf482e3088a0366c113479200
[shaunkenney] <[shaun.kenney@ibm.com]>
2020-04-06 23:42:55 -0400

    minor styling changes

--- src/app/pages/analysis/analysis.component.ts
+++ src/app/pages/analysis/analysis.component.ts
@@ -114,14 +114,14 @@ export class AnalysisComponent implements OnInit {
     const options = {
       hideTabs: true,
       width: "100%",
-      height: "400px",
+      height: "650px",
       onFirstInteractive: function () {
         // The viz is now ready and can be safely used.
         console.log("Run this code when the viz has finished loading.");
       }
 
     };
-    //this.kpiViz = new tableau.Viz(placeholderDiv, url, options);
+    this.kpiViz = new tableau.Viz(placeholderDiv, url, options);
     this.initFilteringForm();
   }
 
