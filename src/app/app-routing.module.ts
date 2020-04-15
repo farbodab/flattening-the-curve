7efd3cbf5669f0afdece673d0f1aa6870791d85c
[shaunkenney] <[shaun.kenney@ibm.com]>
2020-04-14 23:15:19 -0400

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
   { path: 'data', component: DataComponent },
   { path: 'team', component: StoryComponent },
   { path: 'resources', component: ResourcesComponent },
-  { path: 'connect', component: ConnectComponent },
+  //{ path: 'connect', component: ConnectComponent },
   { path: 'outreach', component: OutreachComponent },
   { path: 'blog', component: BlogComponent },
   {
