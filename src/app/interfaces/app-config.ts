58716460b2813e1e123e75544c14b25515ea8486
unknown <farbod.ab@hotmail.ca>
2020-06-13 12:43:22 -0400

    dynamic loading

--- src/app/interfaces/app-config.ts
+++ src/app/interfaces/app-config.ts
@@ -1,4 +1,5 @@
 export class AppConfigProperties {
+    base_url: string;
     results_endpoint: string;
     phu_endpoint: string;
     phunew_endpoint: string;
@@ -6,5 +7,6 @@ export class AppConfigProperties {
     viz_object_endpoint: string;
     plot_object_endpoint: string;
     data_object_endpoint: string;
+    team_object_endpoint: string;
     twitter_object_endpoint: string;
 }
