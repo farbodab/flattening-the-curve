ee00e4b41fe1bd0e7d2e5ff5f4dec16800fa3630
[shaunkenney] <[shaun.kenney@ibm.com]>
2020-06-22 22:06:27 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -74,7 +74,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{ anchorScrolling: 'enabled'})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
