0e3fa68c210722eb7addfa2d2384a2fa98c146f6
skenney518 <spk318@lehigh.edu>
2020-07-12 22:31:03 -0400

    twitter feed embed

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+
+export class FooterComponent implements AfterViewInit {
+    embedLink: string;
+
+    constructor(){
+        this.embedLink="https://twitter.com/howsmyflattenON?ref_src=twsrc%5Etfw" ;
+    }
+
+    ngAfterViewInit() {
+        (<any>window).twttr.widgets.load();
+    }
 }
